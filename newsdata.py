99cda9189ae9a8a1e2127438cb38d3994deedc76
LopezDP <davidplopez@live.com>
2017-09-07 03:17:12 -0400

    Deleted 1st version of queries

--- newsdata.py
+++ newsdata.py
@@ -12,13 +12,6 @@ import psycopg2
 # Query: What are the most popular three articles of all time?
 first_title = "\n1. What are the most popular three articles of all time?\n"
 
-query_1_v_1 = (
-    "select articles.title, count(*) as views "
-    "from articles inner join log on log.path "
-    "like concat('%', articles.slug, '%') "
-    "where log.status like '%200%' group by "
-    "articles.title, log.path order by views desc limit 3")
-
 # 2nd version
 # No need to check status and just count hits
 top_3_articles_sql_query_v_2 = (
@@ -36,13 +29,6 @@ top_3_articles_sql_query_v_3 = (
 # Query: Who are the most popular article authors of all time?
 second_title = "\n2. Who are the most popular article authors of all time?\n"
 
-query_2_v_1 = (
-    "select authors.name, count(*) as views from articles inner "
-    "join authors on articles.author = authors.id inner join log "
-    "on log.path like concat('%', articles.slug, '%') where "
-    "log.status like '%200%' group "
-    "by authors.name order by views desc")
-
 # 2nd version
 # Use any() & string_to_array() to match path to slug
 popular_authors_query_v_2 = (
@@ -53,15 +39,6 @@ popular_authors_query_v_2 = (
 
 # Query: On which days did more than 1% of requests lead to errors
 third_title = "\n3. On which days did more than 1% of requests lead to errors: \n"
-
-query_3_v_1 = (
-    "select day, perc from ("
-    "select day, round((sum(requests)/(select count(*) from log where "
-    "substring(cast(log.time as text), 0, 11) = day) * 100), 2) as "
-    "perc from (select substring(cast(log.time as text), 0, 11) as day, "
-    "count(*) as requests from log where status like '%404%' group by day)"
-    "as log_percentage group by day order by perc desc) as final_query "
-    "where perc >= 1")
 
 # 2nd version
 # Use Sub Select to find Days with errors >= 1%
